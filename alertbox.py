2811aabbf15ca2dbbf0c4ffac1eab1cce062a1cc
Siddhardha <72202100+siddhardhas@users.noreply.github.com>
2021-06-29 20:53:01 +0530

    Delete alertbox.py

--- alertbox.py
+++
@@ -1,6 +0,0 @@
-import pyautogui
-s=input("Don't dare to type yes:")
-if s== 'yes':
-    pyautogui.alert("                 Hello yar,  I said to dont !!! 😯                   ")
-else:
-    print("Good! you listened to me")
