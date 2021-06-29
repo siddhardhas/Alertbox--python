1da0d858df86145c307b44c8f7de830bc88a2604
Siddhardha <72202100+siddhardhas@users.noreply.github.com>
2021-06-29 20:54:54 +0530

    source code

---
+++ alertbox.py
@@ -0,0 +1,6 @@
+import pyautogui
+s=input("Don't dare to type yes:")
+if s== 'yes':
+    pyautogui.alert("                 Hello yar,  I said to dont !!! 😯                   ")
+else:
+    print("Good! you listened to me")
